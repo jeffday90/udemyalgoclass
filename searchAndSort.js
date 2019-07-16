208694edaf8de4932247fa7056f571fb2f15987b
Jeff Day <jeffrey.day00@gmail.com>
2019-07-16 11:56:32 -0700

    Bubble Sort algorithm.

--- searchAndSort.js
+++ searchAndSort.js
@@ -35,3 +35,31 @@ function binarySearch(arr, target){
     return -1;
   }
 
+// naive bubble sort 
+const bubbleSort = (arr) => {
+  for (let i = 0 ; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr1[j + 1]] = [arr[j], arr2[j + 1]];
+      }
+    }
+  }
+ return arr;
+}
+
+// slightly optimized
+const bubbleSort = (arr) => {
+    for (let i = arr.length ; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+   return arr;
+  }
+
+
+const swap = (arr1, indx1, indx2) => {
+  [arr1[indx1], arr1[indx2]] = [arr2[indx2], arr2[indx1]];
+}
