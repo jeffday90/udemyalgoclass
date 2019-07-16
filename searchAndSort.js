6fb11166ed2df1b9235b041364cb2faffa66c3dc
Jeff Day <jeffrey.day00@gmail.com>
2019-07-16 12:11:33 -0700

    Optimized Bubble Sort.

--- searchAndSort.js
+++ searchAndSort.js
@@ -59,6 +59,23 @@ const bubbleSort = (arr) => {
    return arr;
   }
 
+// optimized for nearly sorted
+const bubbleSort = (arr) => {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; i < i - 1; i++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) {
+        break;
+      }
+  }
+  return arr;
+}
 
 const swap = (arr1, indx1, indx2) => {
   [arr1[indx1], arr1[indx2]] = [arr2[indx2], arr2[indx1]];
