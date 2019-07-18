9e5650ecf1599327e1f23a5bed8359f9f6c9c04f
Jeff Day <jeffrey.day00@gmail.com>
2019-07-18 13:15:34 -0700

    Merge sort completed.

--- searchAndSort.js
+++ searchAndSort.js
@@ -144,6 +144,16 @@ const merge = (arr1, arr2) => {
   return arr;
 }
 
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let midPoint = Math.floor(arr.length/2)
+  let left = mergeSort(arr.slice(0, midPoint));
+  let right = mergeSort(arr.slice(midPoint));
+  return merge(left, right);
+}
+
 const swap = (arr1, indx1, indx2) => {
   [arr1[indx1], arr1[indx2]] = [arr1[indx2], arr1[indx1]];
 }
